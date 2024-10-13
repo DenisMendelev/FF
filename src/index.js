bdbe5367f81c9b46bb5f3e43e3abfc79baaed7bf
Denis Mendelev <tawias86@gmail.com>
2024-10-13 17:05:52 +0300

    Updates

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import './index.html';
-import './index.scss';
-import './modules/swiper.js';
-import './modules/burger-menu.js';
-import './modules/feedback.js';
-import './modules/feedback-call.js';
-import './modules/show.js';
